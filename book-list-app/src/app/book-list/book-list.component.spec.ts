3ca693420348dea06c9e9d63dedcb45a5fe03ffa
Barbosa Thaíssa <thathamaria03@gmail.com>
2024-10-06 03:03:46 -0300

    feat: test coverage for `book-list.component` and `book.service`

--- book-list-app/src/app/book-list/book-list.component.spec.ts
+++ book-list-app/src/app/book-list/book-list.component.spec.ts
@@ -1,23 +1,68 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BookListComponent } from './book-list.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { BookService } from '../book.service';
+import { of } from 'rxjs';
+import { Book } from '../book';
 
 describe('BookListComponent', () => {
   let component: BookListComponent;
   let fixture: ComponentFixture<BookListComponent>;
+  let bookService: BookService;
+  let httpTestingController: HttpTestingController;
+  let mockBooks: Book[];
 
   beforeEach(async () => {
+    mockBooks = [
+      {
+        id: 1,
+        title: '1984',
+        author: 'George Orwell',
+        description: 'Dystopian novel',
+      },
+      {
+        id: 2,
+        title: 'To Kill a Mockingbird',
+        author: 'Harper Lee',
+        description: 'Classic of modern American literature',
+      },
+    ];
+
     await TestBed.configureTestingModule({
-      imports: [BookListComponent]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule, BookListComponent], // Importar o componente standalone
+      providers: [BookService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(BookListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    bookService = TestBed.inject(BookService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getBooks and return list of books', () => {
+    spyOn(bookService, 'getBooks').and.returnValue(of(mockBooks));
+    component.getBooks();
+    expect(component.books.length).toBe(2);
+    expect(component.books).toEqual(mockBooks);
+  });
+
+  it('should render book titles in the template', () => {
+    spyOn(bookService, 'getBooks').and.returnValue(of(mockBooks));
+    component.getBooks();
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const bookTitles = compiled.querySelectorAll('.card-title');
+    expect(bookTitles.length).toBe(2);
+    expect(bookTitles[0].textContent).toContain('1984');
+    expect(bookTitles[1].textContent).toContain('To Kill a Mockingbird');
   });
 });
--- book-list-app/src/app/book.service.spec.ts
+++ book-list-app/src/app/book.service.spec.ts
@@ -1,16 +1,55 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { BookService } from './book.service';
 
 describe('BookService', () => {
   let service: BookService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BookService],
+    });
+
     service = TestBed.inject(BookService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch books', () => {
+    const mockBooks = [
+      {
+        id: 1,
+        title: '1984',
+        author: 'George Orwell',
+        description: 'Dystopian novel',
+      },
+      {
+        id: 2,
+        title: 'To Kill a Mockingbird',
+        author: 'Harper Lee',
+        description: 'Classic of modern American literature',
+      },
+    ];
+
+    service.getBooks().subscribe((books) => {
+      expect(books.length).toBe(2);
+      expect(books).toEqual(mockBooks);
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:5000');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockBooks);
   });
 });

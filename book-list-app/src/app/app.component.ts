81ce0523ed72f26919950e5eaac427fcb79ed05a
Barbosa Thaíssa <thathamaria03@gmail.com>
2024-10-06 03:02:03 -0300

    feat: Frontend design finished

--- book-list-app/src/app/app.component.ts
+++ book-list-app/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BookListComponent } from "./book-list/book-list.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BookListComponent],
+  imports: [RouterOutlet, BookListComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
